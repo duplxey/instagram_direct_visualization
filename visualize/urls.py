1bf7cd656c4b7964db658455ce456afa1767b55c
duplxey <nick.tomazic@gmail.com>
2020-10-19 14:02:30 +0200

    Added color palette, get_view.

--- visualize/urls.py
+++ visualize/urls.py
@@ -4,4 +4,5 @@ from visualize import views
 
 urlpatterns = [
     path('', views.index_view, name='visualize-index'),
+    path('get', views.get_view, name='visualize-get'),
 ]
--- visualize/views.py
+++ visualize/views.py
@@ -1,6 +1,16 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def index_view(request):
     return render(request, "visualize/index.html", {})
+
+
+def get_view(request):
+    with open("dump/messages.json", encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+    return JsonResponse(data, safe=False)
 
