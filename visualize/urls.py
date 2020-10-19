e3a704623c743dd8e5089d32dfca56cdc5751e12
duplxey <nick.tomazic@gmail.com>
2020-10-19 14:23:55 +0200

    Loop through all the messages.

--- visualize/urls.py
+++ visualize/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from visualize import views
 
 urlpatterns = [
-    path('', views.index_view, name='visualize-index'),
-    path('get', views.get_view, name='visualize-get'),
+    path("", views.index_view, name='visualize-index'),
+    path("get/", views.get_view, name='visualize-get'),
 ]
